081718d78379dddcb7534c85805825c973a3f537
Rhys <rhysmaddever1@gmail.com>
2025-01-04 19:27:01 +0000

    Update install_and_run.bat

--- install_and_run.bat
+++ install_and_run.bat
@@ -1,7 +1,9 @@
 @echo off
 :: Check if the Visual C++ Redistributable is installed
 VC_redist.x86.exe /quiet /norestart
-echo Installing Visual C++ Redistributable...
-timeout /t 3
+echo Installing Visual C++ Redistributable x86...
+timeout /t 5
+:: Change working directory to the location of the .bat file
+cd /d "%~dp0"
 :: Run the game
 MysteryMaze.exe
